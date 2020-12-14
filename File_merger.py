5e455e93bfef3df7765c3a2f9c128a88008b09e6
Marcin Babalski <marcin.babalski@gmail.com>
2020-12-14 09:43:16 +0100

    working script v0.1

--- File_merger.py
+++ File_merger.py
@@ -1,7 +1,8 @@
 import time
 import os
 date = time.strftime('%Y%m%d')
-file: str = 'Output' + date + '.txt'
+file: str = 'Output_' + date + '.txt'
+
 
 def merge_files(file):
     all_rows: list = []
@@ -9,3 +10,20 @@ def merge_files(file):
     trailer: str = 'Number of rows: '
     filenames = os.listdir('./Input')
 
+    for filename in filenames:
+        with open('./Input/' + filename, "r") as f:
+            for line in f:
+                new_line = line.strip('\n')
+                all_rows.append(new_line)
+
+    with open('./Output/' + file, "w", encoding='utf-8') as f:
+        hdr = header + '\n'
+        f.write(hdr)
+        for item in all_rows:
+            row = item + '|' + '\n'
+            f.write(row)
+        trl = trailer + str(len(all_rows)) + '\n'
+        f.write(trl)
+
+
+merge_files(file)
